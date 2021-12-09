c3b7012bc98da6a996a99af39a7c1f8310b344b6
Nestor <nestorlbp@gmail.com>
2021-12-09 08:15:22 -0500

    Primer commit Envia los primeros datos al front en formato json

---
+++ index.js
@@ -0,0 +1,155 @@
+let express = require("express");
+let cors = require("cors");
+
+let usuariosDB = [
+  {
+    id: "001",
+    nombre: "Nestor",
+    apellido: "Barrios",
+    email: "nestor@gmail.com",
+    telefono: "300-1234567",
+    tipoUsuario: "Configuracion",
+  },
+  {
+    id: "002",
+    nombre: "Keiny",
+    apellido: "Salgado",
+    email: "keiny@gmail.com",
+    telefono: "300-1234567",
+    tipoUsuario: "Gestion",
+  },
+  {
+    id: "003",
+    nombre: "Sandra",
+    apellido: "Gutierrez",
+    email: "sandra@gmail.com",
+    telefono: "300-1234567",
+    tipoUsuario: "Gestion",
+  },
+];
+
+let prediosDB = [
+  {
+    id: "0001",
+    nombre: "Predio 01",
+    Latitud: "11,0084",
+    Longitud: "-74,8216",
+    Usuario: "Nestor",
+    
+  },
+  {
+      id: "0002",
+      nombre: "Predio 02",
+      Latitud: "11,0153",
+      Longitud: "-74,8279",
+      Usuario: "Keiny",
+  },
+  {
+      id: "0003",
+      nombre: "Predio 03",
+      Latitud: "11,0153",
+      Longitud: "-74.7867",
+      Usuario: "Sandra",
+  },
+];
+
+let semillasDB = [
+  {
+    id: "0001",
+    descripcion: "Cereales",
+    CostoAgua: "200",
+    CostoSemilla: "350",
+    CostoFertilizante: "250",
+  },
+  {
+    id: "0002",
+    descripcion: "Leguminosas",
+    CostoAgua: "150",
+    CostoSemilla: "180",
+    CostoFertilizante: "250",
+  },
+  {
+    id: "0003",
+    descripcion: "Hortalizas",
+    CostoAgua: "180",
+    CostoSemilla: "300",
+    CostoFertilizante: "230",
+  },
+];
+
+let cultivosDB = [
+  {
+    id: "001",
+    tipodecultivos: "arroz",
+    area: "lo que sea",
+    cantidadsemillas: "11",
+    tiempodelcultivo: "14 dias",
+    metroscubicosdeagua: "2m3",
+    kgfertilizante: "40kg",
+    tiempopararecoleccion: "5 dias",
+    kgrecolectados: "70",
+  },
+  {
+    id: "002",
+    tipodecultivos: "Yuca",
+    area: "lo que sea",
+    cantidadsemillas: "11",
+    tiempodelcultivo: "14 dias",
+    metroscubicosdeagua: "2m3",
+    kgfertilizante: "40kg",
+    tiempopararecoleccion: "5 dias",
+    kgrecolectados: "70",
+  },
+  {
+    id: "003",
+    tipodecultivos: "Limon",
+    area: "lo que sea",
+    cantidadsemillas: "11",
+    tiempodelcultivo: "14 dias",
+    metroscubicosdeagua: "2m3",
+    kgfertilizante: "40kg",
+    tiempopararecoleccion: "5 dias",
+    kgrecolectados: "70",
+  },
+];
+
+let perfilDB = [
+  {
+    id: "001",
+    nombre: "Sandra Milena",
+    apellido: "Gutierrez",
+    email: "sgutierrez@uninorte.edu.co",
+    telefono: "3009895214",
+    tipoUsuario: "Administrador",
+  }
+];
+
+let corsOptions = {
+  origin: "http://localhost:3000",
+};
+
+const app = express();
+
+app.use("/api/usuarios",cors(corsOptions), (req, res) => {
+  res.json(usuariosDB);
+});
+
+app.use("/api/predios",cors(corsOptions), (req, res) => {
+  res.json(prediosDB);
+});
+
+app.use("/api/semillas",cors(corsOptions), (req, res) => {
+  res.json(semillasDB);
+});
+
+app.use("/api/cultivos",cors(corsOptions), (req, res) => {
+  res.json(cultivosDB);
+});
+
+app.use("/api/perfil",cors(corsOptions), (req, res) => {
+  res.json(perfilDB);
+});
+
+app.listen(8000, () => {
+  console.log("LISTO");
+});
